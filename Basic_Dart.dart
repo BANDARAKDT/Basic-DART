7093e98b6c1ba9e5dadc7ab74b1156a330b4f344
BANDARAKDT <deshanibandarawas@gmail.com>
2025-02-09 11:58:22 +0530

    2025/02/09: Dart

---
+++ Basic_Dart.dart
@@ -0,0 +1,30 @@
+//--------- (01) Main function :
+                  /*The main()function is a predefined method in Dart. It is the most important and mandatory part of any Dart program.Any dart script required the main() method for its execution.This method acts as the entry point for any Dart application.*/
+
+void main(){
+  print("This is the function body of the main function");
+}
+
+//-------- (02) data types : 
+            /*  int(num) =45
+                double(num)=3.145
+                bool= true / false
+                String = "hello"+ 'wolrd'
+                dynamic = 45 / 3.145/ true / 'hello'   [can store any tipe of data]
+            */
+ 
+//-------- (03) Variable : A location in memory that is set aside to store information required by the program.
+            /*   String name ="Deshani Bandara"; 
+                 int age = 25;
+                 bool isProfileLocked =true;
+                 dynamic other = "hellow"
+                 print(name);
+                 print(age);
+                 print(isProfileLocked) */
+ 
+
+
+
+
+
+
