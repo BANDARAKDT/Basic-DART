65809ab2f8159902d955059d8d97f380546e8fce
BANDARAKDT <deshanibandarawas@gmail.com>
2025-02-15 22:59:45 +0530

    2025/02/15 function types

--- functions.dart
+++ functions.dart
@@ -1,9 +1,18 @@
 
+/*
+in dart a function is a reusable block of code that performs a specific task.
+fucntion in dart can be created using the function keyword,followed by the function name, parameter list , and the function body enclosed in curly braces.
+Here is an example of a simple function in dart.  
+*/
+
 
 void main(){
 print("void main() is the main function of the dart");
  int age=25;
  String name="dela";
+ String fullName= 'deshani Bandara';
+print(generateFullName().toUpperCase());
+
 
 //function ---1 calling
 int sum=addTwoNum(5,7);
@@ -17,8 +26,7 @@ print("this is the function to display name $display");
 
 }
 
-
-
+ 
 //function ---1
 int addTwoNum(int num1,int num2){
   return num1+num2;
@@ -27,5 +35,13 @@ int addTwoNum(int num1,int num2){
 
 //function ---2
 String nameDisplay(String name){
-  return name;
+  return name;}
+
+String generateFullName(){
+  String firstName='Deshani';
+  String lastName='Bandara';
+  return '$firstName $lastName';
 }
+
+
+
